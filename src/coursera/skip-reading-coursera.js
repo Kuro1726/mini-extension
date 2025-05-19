71d3fe9c5beffc584882d92fb184780420255b63
unknown <Tuantumat203@gmail.com>
2025-05-19 21:32:07 +0700

    Cải tiến Skip Reading xong Next bài luôn

--- src/coursera/skip-reading-coursera.js
+++ src/coursera/skip-reading-coursera.js
@@ -1,5 +1,12 @@
 const choose = document.querySelector('[data-testid="mark-complete"]')
+const next = document.querySelector('[data-testid="next-item"]')
+
 choose.addEventListener("click", () => {
     console.log ("Đã tự động click");
 })
+next.addEventListener("click", () => {
+    console.log ("Đã tự động click");
+})
+
 choose.click()
+next.click()
